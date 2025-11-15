87b457ab28c05f51e18a01b00d1fda5535980b15
unknown <osemensilas@gmail.com>
2025-11-15 14:18:28 +0100

    New Commit

--- message.php
+++ message.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 
+require __DIR__ . '/vendor/autoload.php';
+
+$enermillApi = "re_ZFP1s3yA_4RkXqGX6mVAq7jRDUQMjJsYx";
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
@@ -31,50 +35,23 @@ if (empty($fullname) || empty($email) || empty($phone) || empty($location) || em
     exit();
 }
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-$mail = new PHPMailer(true);
+$resend = Resend::client($enermillApi);
 
 try {
-    $mail->isSMTP();
-    $mail->Host = 'cloud.webhostingbliss.com'; // Change this to your cPanel SMTP host
-    $mail->SMTPAuth = true;
-    $mail->Username = 'contact@enermillpower.com'; // Your email address
-    $mail->Password = '@bWEEvN[LV@X[Wx7'; // Use App Password if 2FA is enabled
-    $mail->SMTPSecure = 'ssl'; // Use 'ssl' for port 465, 'tls' for port 587
-    $mail->Port = 465; // 465 for SSL, 587 for TLS
+    $resend->emails->send([
+    'from' => 'Enermill <contact@enermill.com>',
+    'to' => ['osemensilas@gmail.com'],
+    'subject' => 'New Message from Enermill',
+    'html' => '<strong>it works!</strong>',
+    ]);
 
-    // Email Headers
-    $mail->setFrom('contact@enermillpower.com', 'Enermill Power Limited'); // Sender
-    $mail->addAddress('osemensilas@gmail.com'); // Recipient ndianaisang@gmail.com
-    $mail->Subject = 'Client From Enermill Power';
-    $mail->Body = 
-        "Name: $fullname\n" .
-        "Phone: $phone\n" .
-        "Email: $email\n" .
-        "Location: $location\n" .
-        "Message: $message";
-
-    // Send Mail
-    if ($mail->send()) {
-        echo json_encode([
-            'status' => 'success',
-            'message' => 'Message sent successfully'
-        ]);
-    } else {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Message not sent. Check connection'
-        ]);
-    }
-} catch (Exception $e) {
+    echo json_encode([
+        'status' => 'successful',
+        'message' => "Message sent successfully"
+    ]);
+} catch (\Throwable $th) {
     echo json_encode([
         'status' => 'error',
-        'message' => "Email failed: {$mail->ErrorInfo}"
+        'message' => $th->getMessage()
     ]);
 }
